eee7f8b5074f5e39bb483dfaf27b61f6c9145dca
BriolVictoria <victoria.briol@student.hepl.be>
2025-03-26 13:38:12 +0100

    Added directory

--- public/index.php
+++ public/index.php
@@ -1,17 +1,14 @@
 <?php
-require __DIR__. '/../vendor/autoload.php';
+
+require __DIR__.'/../vendor/autoload.php';
+require __DIR__.'/../core/helpers/functions.php';
+
+define('VIEW_DIR', __DIR__.'/../resources/views');
+define('CONFIG_DIR', __DIR__.'/../config');
+define('LANG_DIR', __DIR__.'/../lang');
+define('MESSAGES', require LANG_DIR.'/fr/validation.php');
+
 session_start();
 
-
-
-$countries = require '../config/countries.php';
-$pet_types = require '../config/pet_types.php';
-require '../core/helpers/functions.php';
-
-define('VIEW_DIR', __DIR__ . '/../resources/views');
-$router = new Tecgdcs\Router();
+$router = new \Tecgdcs\Router;
 $router->route();
-
-
-$_SESSION['errors'] = null;
-$_SESSION['old'] = null;
